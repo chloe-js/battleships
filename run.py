548f06cb88e7aab512a41318b06746d750699d1c
chloë js <chloejs.horta@gmail.com>
2022-03-27 20:03:37 +0000

    add white space

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from random import randint
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
-letters_to_numbers = {'A':0, 'B':1, 'C':2,'D':3, 'E':4, 'F':5,'G':6, 'H':7}
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2,'D': 3, 'E': 4, 'F': 5,'G': 6, 'H': 7}
 
 def print_board(board):
     '''
