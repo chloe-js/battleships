be02e02c5328b96fbde761e51906406a945980aa
chloë js <chloejs.horta@gmail.com>
2022-03-27 18:13:03 +0000

    CONVERT LETTERS TO NUMBERS

--- run.py
+++ run.py
@@ -3,3 +3,7 @@
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
+letters_to_numbers = {'A':0, 'B':1, 'C':2,'D':3, 'E':4, 'F':5,'G':6, 'H':7}
+
+def board():
+    pass
