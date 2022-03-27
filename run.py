a8c7caf885300de121e60992995d56c896b1a8c0
chloë js <chloejs.horta@gmail.com>
2022-03-27 18:19:17 +0000

    create outlines of funvtion definitions for hidden ship loction and random ship selection

--- run.py
+++ run.py
@@ -6,4 +6,19 @@ GUESS_BOARD = [[' '] * 8 for x in range(8)]
 letters_to_numbers = {'A':0, 'B':1, 'C':2,'D':3, 'E':4, 'F':5,'G':6, 'H':7}
 
 def board():
+    '''
+    Create board to see
+    '''
+    pass
+
+def create_ships():
+    '''
+    Random hidden ships for user to find
+    '''
+    pass
+
+def get_ship_loction():
+    '''
+    Ask user what row and colomn they want to choose to find the ship
+    '''
     pass
