fcbc845e12ba1ceb5f430b23bd3cce2500b29924
chloë js <chloejs.horta@gmail.com>
2022-03-27 18:35:05 +0000

    for each row we are itering through on the board we are going to join a separator

--- run.py
+++ run.py
@@ -5,12 +5,17 @@ GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
 letters_to_numbers = {'A':0, 'B':1, 'C':2,'D':3, 'E':4, 'F':5,'G':6, 'H':7}
 
-def board():
+def print_board(board):
     '''
-    Create board to see
+    Create board to see where user can guess where to hit
     '''
-    pass
-
+    print('  A B C D E F G H')
+    print('  ---------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+    
 def create_ships():
     '''
     Random hidden ships for user to find
